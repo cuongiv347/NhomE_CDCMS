48253cfcbaf952f9c879def07b4d1a78b014f46d
cuongiv347 <pccuong112@gmail.com>
2019-10-28 10:13:55 +0700

    Cuong

---
+++ Module_cuong/830-content.php
@@ -0,0 +1,94 @@
+
+    <div class="preloader">
+        <div class="spinner">
+            <div class="pre-bounce1"></div>
+            <div class="pre-bounce2"></div>
+        </div>
+    </div>
+
+    <div id="page" class="hfeed site">
+        <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+
+
+        <header id="masthead" class="site-header" role="banner">
+            <div class="header-wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-8 col-xs-12">
+                            <h1 class="site-title"><a href="https://demo.athemes.com/bistro/" rel="home">Bistro</a></h1>
+                            <h2 class="site-description"></h2>
+                        </div>
+                        <div class="col-md-8 col-sm-4 col-xs-12">
+                            <div class="btn-menu"></div>
+                            <nav id="mainnav" class="mainnav" role="navigation">
+                                <div class="menu-menu-1-container">
+                                    <ul id="menu-menu-1" class="menu">
+                                        <li id="menu-item-61"
+                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-4 current_page_item menu-item-61">
+                                            <a href="https://demo.athemes.com/bistro/" aria-current="page">Home</a></li>
+                                        <li id="menu-item-65"
+                                            class="menu-item menu-item-type-taxonomy menu-item-object-restaurant_tag menu-item-65">
+                                            <a href="https://demo.athemes.com/bistro/menu/tags/dinner/">Dinner</a></li>
+                                        <li id="menu-item-66"
+                                            class="menu-item menu-item-type-taxonomy menu-item-object-restaurant_tag menu-item-66">
+                                            <a href="https://demo.athemes.com/bistro/menu/tags/lunch/">Lunch</a></li>
+                                        <li id="menu-item-69"
+                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-69">
+                                            <a href="https://demo.athemes.com/bistro/about/">About</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
+        <div class="sydney-hero-area">
+
+            <div id="slideshow" class="header-slider" data-speed="4000">
+                <div class="slides-container">
+                    <div class="slide-item"
+                        style="background-image:url(https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/bistro/images/1.jpg);">
+                    </div>
+                    <div class="slide-item"
+                        style="background-image:url(https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/bistro/images/2.jpg);">
+                    </div>
+                </div>
+
+                <div class="text-slider-section">
+                    <div class="text-slider">
+                        <ul class="slide-text slides">
+                            <li>
+                                <div class="contain">
+                                    <h2 class="maintitle">Welcome to our bistro</h2>
+                                    <p class="subtitle">Feel free to look around</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="contain">
+                                    <h2 class="maintitle">Ready to see what we offer?</h2>
+                                    <p class="subtitle">Click the button below</p>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <a href="#primary" class="roll-button button-slider">Click to begin</a>
+                </div>
+            </div>
+            <div class="header-image">
+                <div class="overlay"></div> <img class="header-inner"
+                    src="https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/uploads/sites/38/2015/05/cropped-kitchen-731351_1280.jpg"
+                    width="1920" alt="Bistro" title="Bistro">
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+   
---
+++ Module_cuong/830.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="https://demo.athemes.com/bistro/xmlrpc.php">
+
+    <title>Bistro</title>
+    <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+    <link rel='dns-prefetch' href='//s.w.org' />
+    <link rel="alternate" type="application/rss+xml" title="Bistro &raquo; Feed"
+        href="https://demo.athemes.com/bistro/feed/" />
+    <link rel="alternate" type="application/rss+xml" title="Bistro &raquo; Comments Feed"
+        href="https://demo.athemes.com/bistro/comments/feed/" />
+    <link rel="alternate" type="application/rss+xml" title="Bistro &raquo; My front page Comments Feed"
+        href="https://demo.athemes.com/bistro/my-front-page/feed/" />
+    <script type="text/javascript">
+    window._wpemojiSettings = {
+        "baseUrl": "https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/72x72\/",
+        "ext": ".png",
+        "svgUrl": "https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/svg\/",
+        "svgExt": ".svg",
+        "source": {
+            "concatemoji": "https:\/\/demo.athemes.com\/bistro\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.2.4"
+        }
+    };
+    ! function(a, b, c) {
+        function d(a, b) {
+            var c = String.fromCharCode;
+            l.clearRect(0, 0, k.width, k.height), l.fillText(c.apply(this, a), 0, 0);
+            var d = k.toDataURL();
+            l.clearRect(0, 0, k.width, k.height), l.fillText(c.apply(this, b), 0, 0);
+            var e = k.toDataURL();
+            return d === e
+        }
+
+        function e(a) {
+            var b;
+            if (!l || !l.fillText) return !1;
+            switch (l.textBaseline = "top", l.font = "600 32px Arial", a) {
+                case "flag":
+                    return !(b = d([55356, 56826, 55356, 56819], [55356, 56826, 8203, 55356, 56819])) && (b = d([55356,
+                        57332, 56128, 56423, 56128, 56418, 56128, 56421, 56128, 56430, 56128, 56423, 56128,
+                        56447
+                    ], [55356, 57332, 8203, 56128, 56423, 8203, 56128, 56418, 8203, 56128, 56421, 8203,
+                        56128, 56430, 8203, 56128, 56423, 8203, 56128, 56447
+                    ]), !b);
+                case "emoji":
+                    return b = d([55357, 56424, 55356, 57342, 8205, 55358, 56605, 8205, 55357, 56424, 55356, 57340], [
+                        55357, 56424, 55356, 57342, 8203, 55358, 56605, 8203, 55357, 56424, 55356, 57340
+                    ]), !b
+            }
+            return !1
+        }
+
+        function f(a) {
+            var c = b.createElement("script");
+            c.src = a, c.defer = c.type = "text/javascript", b.getElementsByTagName("head")[0].appendChild(c)
+        }
+        var g, h, i, j, k = b.createElement("canvas"),
+            l = k.getContext && k.getContext("2d");
+        for (j = Array("flag", "emoji"), c.supports = {
+                everything: !0,
+                everythingExceptFlag: !0
+            }, i = 0; i < j.length; i++) c.supports[j[i]] = e(j[i]), c.supports.everything = c.supports.everything && c
+            .supports[j[i]], "flag" !== j[i] && (c.supports.everythingExceptFlag = c.supports.everythingExceptFlag && c
+                .supports[j[i]]);
+        c.supports.everythingExceptFlag = c.supports.everythingExceptFlag && !c.supports.flag, c.DOMReady = !1, c
+            .readyCallback = function() {
+                c.DOMReady = !0
+            }, c.supports.everything || (h = function() {
+                c.readyCallback()
+            }, b.addEventListener ? (b.addEventListener("DOMContentLoaded", h, !1), a.addEventListener("load", h, !
+                1)) : (a.attachEvent("onload", h), b.attachEvent("onreadystatechange", function() {
+                "complete" === b.readyState && c.readyCallback()
+            })), g = c.source || {}, g.concatemoji ? f(g.concatemoji) : g.wpemoji && g.twemoji && (f(g.twemoji), f(g
+                .wpemoji)))
+    }(window, document, window._wpemojiSettings);
+    </script>
+
+    <link rel='stylesheet' id='sydney-bootstrap-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/sydney/css/bootstrap/bootstrap.min.css?ver=1'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='wp-block-library-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-includes/css/dist/block-library/style.min.css?ver=5.2.4'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='contact-form-7-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.1.4'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='siteorigin-panels-front-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/plugins/siteorigin-panels/css/front-flex.min.css?ver=2.10.11'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='bistro-parent-style-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/sydney/style.css?ver=5.2.4' type='text/css'
+        media='all' />
+    <link rel='stylesheet' id='sydney-fonts-css'
+        href='https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C400italic%2C600%7CRaleway%3A400%2C500%2C600'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='sydney-style-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/bistro/style.css?ver=20180710'
+        type='text/css' media='all' />
+
+    <link rel='stylesheet' href='./css/test.css' />
+
+
+    <link rel='stylesheet' id='sydney-font-awesome-css'
+        href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/sydney/fonts/font-awesome.min.css?ver=5.2.4'
+        type='text/css' media='all' />
+    <!--[if lte IE 9]>
+<link rel='stylesheet' id='sydney-ie9-css'  href='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/sydney/css/ie9.css?ver=5.2.4' type='text/css' media='all' />
+<![endif]-->
+    <script type='text/javascript'
+        src='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+    <script type='text/javascript'
+        src='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var wpgmza_google_api_status = {
+        "message": "Enqueued",
+        "code": "ENQUEUED"
+    };
+    /* ]]> */
+    </script>
+    <script type='text/javascript'
+        src='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/plugins/wp-google-maps/wpgmza_data.js?ver=5.2.4'>
+    </script>
+    <link rel='https://api.w.org/' href='https://demo.athemes.com/bistro/wp-json/' />
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://demo.athemes.com/bistro/xmlrpc.php?rsd" />
+    <link rel="wlwmanifest" type="application/wlwmanifest+xml"
+        href="https://demo.athemes.com/bistro/wp-includes/wlwmanifest.xml" />
+    <meta name="generator" content="WordPress 5.2.4" />
+    <link rel="canonical" href="https://demo.athemes.com/bistro/" />
+    <link rel='shortlink' href='https://demo.athemes.com/bistro/' />
+    <link rel="alternate" type="application/json+oembed"
+        href="https://demo.athemes.com/bistro/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fdemo.athemes.com%2Fbistro%2F" />
+    <link rel="alternate" type="text/xml+oembed"
+        href="https://demo.athemes.com/bistro/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fdemo.athemes.com%2Fbistro%2F&#038;format=xml" />
+
+    <!-- Google Webmaster Tools plugin for WordPress -->
+
+
+ 
+</head>
+<body
+    class="home page-template page-template-page-templates page-template-page_front-page page-template-page-templatespage_front-page-php page page-id-4 siteorigin-panels siteorigin-panels-before-js siteorigin-panels-home">
+
+    
+    <?php include './830-content.php'; ?>
+
+
+    <script type='text/javascript'
+        src='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/sydney/js/scripts.js?ver=5.2.4'></script>
+    <script type='text/javascript'
+        src='https://mk0athemesdemon3j7s5.kinstacdn.com/wp-content/themes/sydney/js/main.min.js?ver=20180716'></script>
+
+
+
+
+</body>
+
+</html>
